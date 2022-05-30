d65c2e4c24abea2f1701757d6e517f5d8eb576ed
gervic08 <germanvicentin@gmail.com>
2022-05-30 11:28:04 -0300

    Authentication feature created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username]) 
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You´re successfully logged in."
+      redirect_to root_path
+    else
+      flash[:alert] = "Invalid credentials. Please try again."
+      render 'new'
+    end 
+  end
+end
+
+def destroy
+  session[:user_id] = nil
+  flash[:alert] = "Succesfully logged out"
+  redirect_to root_path
 end
