d7ce77894ba1091bef917d84968b2b9150ff5d07
gervic08 <germanvicentin@gmail.com>
2022-05-31 20:05:56 -0300

    Heroku Deploy configiration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  config.web_socket_server_url = 'wss://gervic-chattingapp.herokuapp.com/api/v1/cable'
+  config.action_cable.allowed_request_origins = ['https://gervic-chattingapp.herokuapp.com/']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
